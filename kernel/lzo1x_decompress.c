9eb12cc290fb2f03f80ba7914824b1532060a732
Vasilis Vasaitis <vasilis@acunu.com>
2012-05-16 12:04:27 +0100

    modify the LZO source files in order to build with castle
    --HG--
    extra : rebase_source : 6959d6026b3638b04eead0c8574a3c91b8f77351

--- kernel/lzo1x_decompress.c
+++ kernel/lzo1x_decompress.c
@@ -17,7 +17,7 @@
 #endif
 
 #include <asm/unaligned.h>
-#include <linux/lzo.h>
+#include "lzo.h"
 #include "lzodefs.h"
 
 #define HAVE_IP(x, ip_end, ip) ((size_t)(ip_end - ip) < (x))
@@ -26,6 +26,8 @@
 
 #define COPY4(dst, src)	\
 		put_unaligned(get_unaligned((const u32 *)(src)), (u32 *)(dst))
+
+#define get_unaligned_le16(x)   le16_to_cpu(get_unaligned(x))
 
 int lzo1x_decompress_safe(const unsigned char *in, size_t in_len,
 			unsigned char *out, size_t *out_len)
